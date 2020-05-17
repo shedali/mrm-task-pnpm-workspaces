a36478a2beb9b956b629e8088636c90a8c66aa4a
Franz <shedali@gmail.com>
2020-05-17 22:05:23 +0100

    update name

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const { json, packageJson, lines, install } = require("mrm-core");
+//add file pnpmworkspace
 
 function task() {
   const file = json("./package.json");
   file.exists();
-  install("@ava/typescript");
-  install("ava");
+  // install("@ava/typescript");
+  // install("ava");
   const pkg = packageJson()
-    .setScript("test", "ava")
-    .setScript("test:watch", "ava --watch")
-    .save();
-  file.merge({
-    ava: {
-      typescript: {
-        extensions: ["ts", "tsx"],
-        rewritePaths: {
-          "src/": "dist/"
-        }
-      }
-    }
-  });
+  .setScript("test", "pnpm recursive run test")
+  .setScript("dev", "pnpm recursive run dev")
+  .setScript("build", "pnpm recursive run build")
+  .save();
+
+  lines('pnpm-workspace.yaml').add(`
+    packages:
+    - '**'
+    `).save();
+
+  lines('.npmrc').add('link-workspace-packages = true')
+  
   file.save();
 }
 
-task.description = "Adds ava config";
+task.description = "adds pnpm workspace";
 module.exports = task;
