1511ad929c0a666de02b9e77a71309101c3ee609
Franz Sittampalam <shedali@gmail.com>
2021-08-03 19:16:56 +0100

    make packages dir

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const { json, packageJson, lines, install } = require("mrm-core");
+const { json, packageJson, lines, install, makeDirs } = require("mrm-core");
 
 function task() {
   const pkg = packageJson()
-  .setScript("test", "pnpm recursive run ci")
-  .setScript("dev", "pnpm recursive run dev")
-  .setScript("build", "pnpm recursive run build")
-  .save();
+    .setScript("test", "pnpm recursive run ci")
+    .setScript("dev", "pnpm recursive run dev")
+    .setScript("build", "pnpm recursive run build")
+    .save();
 
-  lines('pnpm-workspace.yaml')
-  .add('packages:')
-  .add("  - '**'")
-  .save();
-
-  lines('.npmrc')
-  .add('link-workspace-packages=true').save();
+  makeDirs("packages");
+  lines("pnpm-workspace.yaml").add("packages:").add("  - '**'").save();
+  lines(".npmrc").add("link-workspace-packages=true").save();
 }
 
 task.description = "adds pnpm workspace";
